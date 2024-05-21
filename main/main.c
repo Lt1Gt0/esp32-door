9c8491b16bc137b74a19e0b1e2a5e5ec957de772
Lt1Gt0 <simonkaminski1100@gmail.com>
2024-05-20 17:09:25 -0700

    Added todo comments for future implementation needs

--- main/main.c
+++ main/main.c
@@ -1,3 +1,19 @@
+/* TODO
+ * Configure LED to GPIO for testing
+ * Add route to web server to toggle led on or off (debug)
+ * Connect relay to esp32
+ * Connect motor to relay and ESP32 circuit
+ * Control Motor from webserver
+ *  Toggle Spin direction of motor from web server
+ *  Show status of motor spinning or not spinning
+ *  Show direction of spin from web server
+ *  Determine if the motor is unable to turn (e.g. door closed so dont try to break motor (or door) to spin)
+ *  stop motor while spinning if wanted
+ *  Detect position of motor in relation to door position
+ *  Calibration sequence
+ *  Show angle of motor??? (0 -> 360)
+ */
+
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
